a21e6ffebb140c6a2af0d078cc034ab90883cbd8
ArpitaPanda261 <arpitapanda261@gmail.com>
2024-06-20 21:31:14 +0530

    Flast List Completed

--- src/libs/screenComponents/sware-home-component/organisms/HomeScreen.js
+++ src/libs/screenComponents/sware-home-component/organisms/HomeScreen.js
@@ -1,30 +1,48 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import FormScreenA from './FormScreenA';
 import FormScreenJ from './FormScreenJ';
 import CardScreenA from './CardScreenA';
 import CamCardA from './CamCardA';
+import PropertiesA from './PropertiesA';
+import FlatListA from './FlatListA';
 
 const HomeScreen = () => {
+  const data = [
+    { id: 'formA', component: <FormScreenA /> },
+    { id: 'cardA', component: <CardScreenA /> },
+    { id: 'camCardA', component: <CamCardA /> },
+    { id: 'propertiesTheme', component: <PropertiesA title="Theme" content="Theme plays the main role in designing." /> },
+    { id: 'propertiesColor', component: <PropertiesA title="Color" content="Makes things attractive." /> },
+    { id: 'propertiesImages', component: <PropertiesA title="Images" content="Makes people emotionally involved." /> },
+    { id: 'flatListA', component: <FlatListA /> },
+    { id: 'formJ', component: <FormScreenJ /> },
+  ];
+
+  const renderItem = ({ item }) => (
+    <View style={styles.itemContainer}>
+      {item.component}
+    </View>
+  );
 
   return (
-    <ScrollView contentContainerStyle={styles.scrollContainer}>
-    <FormScreenA />
-    <CardScreenA />
-    <CamCardA />
-    <FormScreenJ />
-    </ScrollView>
+    <FlatList
+      data={data}
+      renderItem={renderItem}
+      keyExtractor={item => item.id}
+      contentContainerStyle={styles.container}
+    />
   );
 };
 
 const styles = StyleSheet.create({
-  scrollContainer: {
+  container: {
     flexGrow: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     padding: 16,
   },
- 
+  itemContainer: {
+    marginBottom: 16,
+  },
 });
 
 export default HomeScreen;
